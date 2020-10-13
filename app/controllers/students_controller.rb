d190e4ddf661fa10b57940a6e52341373626e706
mariusmoldovan <biris_21@yahoo.com>
2020-10-12 19:20:04 -0500

    rails url finish

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,8 +8,18 @@ class StudentsController < ApplicationController
   def show
   end
 
-  private
-
+  
+  def activate
+    @student = Student.find(params[:id])
+    if @student.active
+      @student.active = false
+    else
+      @student.active = true
+    end
+    @student.save
+    redirect_to student_path(@student)
+  end
+private
     def set_student
       @student = Student.find(params[:id])
     end
